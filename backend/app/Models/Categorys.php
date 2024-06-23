816bc6e714eb361abb8c329c679ed491497a1830
CHANNICH NOEURN <annenichz1507>
2024-06-23 13:42:35 +0700

    crud category #GP-54

--- backend/app/Models/Categorys.php
+++ backend/app/Models/Categorys.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Categorys extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'description']; 
+
+    public static function list(){
+        return self::all();
+    }
+    public static function store($request, $id = null){
+        $data = $request->only('name', 'description');
+        $data = self::updateOrCreate(['id' => $id], $data);
+        return $data;
+        
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Front\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,13 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/me', [AuthController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/post/list', [PostController::class, 'index'])->middleware('auth:sanctum');
+
+
+// CATEGORY ROUTES
+Route::prefix('category')->group(function () {
+    Route::get('/list', [CategoryController::class, 'index']);
+    Route::post('/create', [CategoryController::class, 'store']);
+    Route::get('/show/{id}', [CategoryController::class, 'show']);
+    Route::put('/update/{id}', [CategoryController::class, 'update']);
+    Route::delete('/delete/{id}', [CategoryController::class, 'destroy']);
+});
