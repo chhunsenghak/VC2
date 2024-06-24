de695088e427171889847618ab664c1c4969e54d
library <senghak.chhun@student.passerellesnumeriques.org>
2024-06-24 13:55:32 +0700

    [feat] update route api #GP-59

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -72,6 +72,7 @@ class AuthController extends Controller
             return response()->json([
                 'status' => true,
                 'message' => 'User Created Successfully',
+                'user'=> $user,
                 'token' => $user->createToken("API TOKEN")->plainTextToken
             ], 200);
         } catch (\Throwable $th) {
