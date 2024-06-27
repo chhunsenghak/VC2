d97a7e0f7cbfdeef431ca3d5807df4d1be70d266
SOKUNTHEA.POEUN <SOKUNTHEA.POEUN@PNC.LAN>
2024-06-27 20:21:39 +0700

    switch branch

--- frontend/src/views/Web/shop/ShopView.vue
+++ frontend/src/views/Web/shop/ShopView.vue
@@ -1,52 +1,123 @@
 <template>
     <WebLayout>
 
-        <!-- Start Popular Product -->
-        <div class="popular-product">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-                        <div class="product-item-sm d-flex">
-                            <div class="thumbnail">
-                                <img src="/src/assets/images/product-1.png" alt="Image" class="img-fluid" />
-                            </div>
-                            <div class="pt-3">
-                                <h3>Tomato</h3>
-                                <p>Fresh tomato it is from Battambang Province with high quality</p>
-                                <p><a href="#">Read More</a></p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-                        <div class="product-item-sm d-flex">
-                            <div class="thumbnail">
-                                <img src="/src/assets/images/product-2.png" alt="Image" class="img-fluid" />
-                            </div>
-                            <div class="pt-3">
-                                <h3>Kompot Durian</h3>
-                                <p>This fruit actully from Kompot province sweet and good taste</p>
-                                <p><a href="#">Read More</a></p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-md-6 col-lg-4 mb-4 mb-lg-0">
-                        <div class="product-item-sm d-flex">
-                            <div class="thumbnail">
-                                <img src="/src/assets/images/product-3.png" alt="Image" class="img-fluid" />
-                            </div>
-                            <div class="pt-3">
-                                <h3>Meat</h3>
-                                <p>This meat is in Cambodia that feed by farmer in countryside</p>
-                                <p><a href="#">Read More</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <!-- Start Testimonial Slider -->
+    <div class="testimonial-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 mx-auto text-center">
+            <h2 class="section-title">User Feedback</h2>
+          </div>
         </div>
-        <!-- End Popular Product -->
+
+        <div class="row justify-content-center">
+          <div class="col-lg-12">
+            <div class="testimonial-slider-wrap text-center">
+              <div id="testimonial-nav">
+                <span class="prev" data-controls="prev"
+                  ><span class="fa fa-chevron-left"></span
+                ></span>
+                <span class="next" data-controls="next"
+                  ><span class="fa fa-chevron-right"></span
+                ></span>
+              </div>
+
+              <div class="testimonial-slider">
+                <div class="item">
+                  <div class="row justify-content-center">
+                    <div class="col-lg-8 mx-auto">
+                      <div class="testimonial-block text-center">
+                        <blockquote class="mb-5">
+                          <p>
+                            &ldquo;This website is the best for me and for other farmer we can
+                            sellers we can buy food for daily. I can eat fresh vegetable, fruit that
+                            that I buy from Phsakasekor.&rdquo;
+                          </p>
+                        </blockquote>
+
+                        <div class="author-info">
+                          <div class="author-pic">
+                            <img
+                              src="/src/assets/images/vanny-3.png"
+                              alt="Vanny Chan"
+                              class="img-fluid"
+                            />
+                          </div>
+                          <h3 class="font-weight-bold">Vanny Chan</h3>
+                          <span class="position d-block mb-3">CEO and project manager at PNC.</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- END item -->
+
+                <div class="item">
+                  <div class="row justify-content-center">
+                    <div class="col-lg-8 mx-auto">
+                      <div class="testimonial-block text-center">
+                        <blockquote class="mb-5">
+                          <p>
+                            &ldquo;What's a great website I had not ever seen before It's really
+                            easy to use for all people. this website it focus on natural, healthy,
+                            and quality of product.&rdquo;
+                          </p>
+                        </blockquote>
+
+                        <div class="author-info">
+                          <div class="author-pic">
+                            <img
+                              src="/src/assets/images/person-1.png"
+                              alt="Tiv Ngout"
+                              class="img-fluid"
+                            />
+                          </div>
+                          <h3 class="font-weight-bold">Tiv Ngout</h3>
+                          <span class="position d-block mb-3">CEO of APD bank.</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- END item -->
+
+                <div class="item">
+                  <div class="row justify-content-center">
+                    <div class="col-lg-8 mx-auto">
+                      <div class="testimonial-block text-center">
+                        <blockquote class="mb-5">
+                          <p>
+                            &ldquo;The website is user-friendly, with categories that make it easy
+                            to navigate and find products tailored to different dietary preferences
+                            and health needs.&rdquo;
+                          </p>
+                        </blockquote>
+
+                        <div class="author-info">
+                          <div class="author-pic">
+                            <img
+                              src="/src/assets/images/neardey-2.png"
+                              alt="Neardey Loem"
+                              class="img-fluid"
+                            />
+                          </div>
+                          <h3 class="font-weight-bold">Neardey Loem</h3>
+                          <span class="position d-block mb-3"
+                            >Seller at Toul Tum Poung market.</span
+                          >
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- END item -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Testimonial Slider -->
     </WebLayout>
 
 </template>
