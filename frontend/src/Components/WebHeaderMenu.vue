04b813055f49ad1dd7736b89c9aa9538d777ee3f
RADY.DOEURN <RADY.DOEURN@PNC.LAN>
2024-06-27 16:08:09 +0700

    switch branch

--- frontend/src/Components/WebHeaderMenu.vue
+++ frontend/src/Components/WebHeaderMenu.vue
@@ -5,22 +5,22 @@ import { Icon } from '@iconify/vue'
   <header class="flex justify-between px-50 py-3 bg-white items-center m-0">
     <!-- Logo -->
     <div class="flex items-center space-x-2">
-      <img src="../image/logo1.png" alt="" width="100%" height="80vh">
+      <img src="../image/logo2.jpg" alt="" width="80px" height="80px">
     </div>
 
     <!-- Menu Items -->
     <nav class="flex justify-center gap-5 space-x-4 " id="nav">
-      <router-link class="font-bold px-2 py-2 text-dark-700 fs-5 rounded-lg hover:text-blue-700" to="/post"
+      <router-link class="font-bold px-2 py-2 text-dark-700 fs-6 rounded-lg hover:text-blue-700" to="/post"
         style="text-decoration: none;">HOME</router-link>
-      <router-link class="font-bold px-2 py-2 text-dark-700 fs-5 rounded-lg  hover:text-blue-700" to=""
+      <router-link class="font-bold px-2 py-2 text-dark-700 fs-6 rounded-lg  hover:text-blue-700" to=""
         style="text-decoration: none;">SHOP</router-link>
-      <router-link class="font-bold px-2 py-2 text-dark-700 fs-5 rounded-lg  hover:text-blue-700" to=""
+      <router-link class="font-bold px-2 py-2 text-dark-700 fs-6 rounded-lg  hover:text-blue-700" to=""
         style="text-decoration: none;">ABOUT US</router-link>
-      <router-link class="font-bold px-2 py-2 text-dark-700 fs-5 rounded-lg  hover:text-blue-700" to=""
+      <router-link class="font-bold px-2 py-2 text-dark-700 fs-6 rounded-lg  hover:text-blue-700" to=""
         style="text-decoration: none;">SERVICE</router-link>
-      <router-link class="font-bold px-2 py-2 text-dark-700 fs-5 rounded-lg  hover:text-blue-700" to=""
+      <router-link class="font-bold px-2 py-2 text-dark-700 fs-6 rounded-lg  hover:text-blue-700" to=""
         style="text-decoration: none;">BLOG</router-link>
-      <router-link class="font-bold px-2 py-2 text-dark-700 fs-5 rounded-lg  hover:text-blue-700" to=""
+      <router-link class="font-bold px-2 py-2 text-dark-700 fs-6 rounded-lg  hover:text-blue-700" to=""
         style="text-decoration: none;">CONTACT US</router-link>
     </nav>
 
