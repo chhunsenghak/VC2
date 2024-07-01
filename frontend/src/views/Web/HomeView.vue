81ad6668473e002621b335dafa56e5580eee8ce5
CEO of Bathroom <141013479+chhuneii2004@users.noreply.github.com>
2024-07-01 07:52:32 +0700

    [imp] change style and correct code structure GP-64

--- frontend/src/views/Web/HomeView.vue
+++ frontend/src/views/Web/HomeView.vue
@@ -4,32 +4,22 @@ import HeroComponent from '../../../src/Components/HomePageComponents/HeroCompon
 import WhyChooseUsComponent from '../../../src/Components/HomePageComponents/WhyChooseUsComponent.vue';
 import ProductComponent from '../../../src/Components/HomePageComponents/ProductComponent.vue';
 import WeHelpComponent from '../../../src/Components/HomePageComponents/WeHelpComponent.vue';
-import PopularProductComponent from '../../../src/Components/HomePageComponents/PopularProductComponent.vue';
-import TestimonialSliderComponent from '../../../src/Components/HomePageComponents/TestimonialSliderComponent.vue';
-import BlogComponent from '../.././../src/Components/HomePageComponents/BlogComponent.vue';
+
 </script>
 <template>
   <WebLayout>
     <!-- Start Hero Section -->
     <HeroComponent />
     <!-- end Hero Section -->
-
-    <!-- Start Why Choose Us Section -->
-    <WhyChooseUsComponent />
     <!-- Start Product Section -->
     <ProductComponent />
     <!-- End Product Section -->
+    <!-- Start Why Choose Us Section -->
+    <WhyChooseUsComponent />
+     <!-- Start Why Choose Us Section -->
     <!-- Start We Help Section -->
     <WeHelpComponent />
     <!-- End We Help Section -->
-    <!-- Start Popular Product -->
-    <PopularProductComponent />
-    <!-- End Popular Product -->
-    <!-- Start Testimonial Slider -->
-      <TestimonialSliderComponent/>
-    <!-- End Testimonial Slider -->
-    <!-- Start Blog Section -->
-     <BlogComponent/>
-    <!-- End Blog Section -->
+
   </WebLayout>
 </template>
