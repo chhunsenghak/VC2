e6e031f6f9ab88436d3b7c3bfa0d8a73f4665878
library <senghak.chhun@student.passerellesnumeriques.org>
2024-07-02 10:22:29 +0700

    [feat] merge code form shop pae GP-154

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -50,7 +50,7 @@ const router = createRouter({
 })
 
 router.beforeEach(async (to, from, next) => {
-  const publicPages = ['/register', '/login']
+  const publicPages = ['/','/register', '/login']
   const authRequired = !publicPages.includes(to.path)
   const store = useAuthStore()
   try {
