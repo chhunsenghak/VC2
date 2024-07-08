353625e1109ade8f914ffabda5f759c9f4423df8
library <senghak.chhun@student.passerellesnumeriques.org>
2024-07-08 10:59:47 +0700

    [feat] add route link to my profile page GP-184

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -67,6 +67,11 @@ const router = createRouter({
       component: () => import('@/views/Web/Post/ServiceView.vue')
     },
     {
+      path: '/profile',
+      name: 'profilePage',
+      component: () => import('@/views/Web/Profile/UserProfile.vue')
+    },
+    {
       path: '/myProducts',
       name: 'myProducts',
       component: () => import('@/views/Web/Product/MyProducts.vue')
@@ -75,7 +80,7 @@ const router = createRouter({
 })
 
 router.beforeEach(async (to, from, next) => {
-  const publicPages = ['/', '/register', '/login', '/shop', '/contact_us', '/about_us', '/forgot_password', '/reset_password','/services']
+  const publicPages = ['/', '/register', '/login', '/shop', '/contact_us', '/about_us', '/forgot_password', '/reset_password', '/services']
   const authRequired = !publicPages.includes(to.path)
   const store = useAuthStore();
   try {
