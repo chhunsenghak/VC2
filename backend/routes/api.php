bd02f195bd0ec0168f90bed07abef2adc686b7f5
CEO of Bathroom <141013479+chhuneii2004@users.noreply.github.com>
2024-07-12 09:37:30 +0700

    switch branch only

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace App\Http\Controllers;
 use App\Http\Controllers\Admin\CommuneController;
 use App\Http\Controllers\Admin\DistrictController;
 use App\Http\Controllers\Admin\ProvinceController;
